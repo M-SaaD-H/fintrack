561216ee87adfd67b3465428764c92c57bcec402
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-03-23 12:43:18 +0530

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/sign-in', request.url))
   }
 
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
