39bceb3fe5ae90578dfd715bd151c3fa9bc8f52f
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-04-04 23:42:11 +0530

    deployed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className=" text-9xl font-bold">Fintrack</h1>
-      <p className="text-lg text-muted-foreground mt-2">Take Control of Your Finances - Track, Save, and Grow!</p>
+      <h1 className="text-6xl md:text-9xl font-bold">Fintrack</h1>
+      <p className="text-base md:text-lg text-muted-foreground mt-2">Take Control of Your Finances - Track, Save, and Grow!</p>
       <Link href="/sign-in" className="mt-8">
-        <Button className="py-6 px-7 text-base">Get Started</Button>
+        <Button className="md:py-6 md:px-7 md:text-base">Get Started</Button>
       </Link>
     </div>
   );
