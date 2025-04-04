e54f4129d8d3755e5647288c043c93d2b5fb6f63
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-04-04 23:46:09 +0530

    deployed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-6xl md:text-9xl font-bold">Fintrack</h1>
-      <p className="text-base md:text-lg text-muted-foreground mt-2">Take Control of Your Finances - Track, Save, and Grow!</p>
+      <p className="mx-8 text-center text-base md:text-lg text-muted-foreground mt-4">Take Control of Your Finances - Track, Save, and Grow!</p>
       <Link href="/sign-in" className="mt-8">
         <Button className="md:py-6 md:px-7 md:text-base">Get Started</Button>
       </Link>
