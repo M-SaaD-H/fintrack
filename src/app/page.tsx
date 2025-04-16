fbc82c33874d5802dbc72be5ec80f5c2251c36f4
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-04-17 00:45:11 +0530

    hero and navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl md:text-9xl font-bold">Fintrack</h1>
-      <p className="mx-8 text-center text-base md:text-lg text-muted-foreground mt-4">Take Control of Your Finances - Track, Save, and Grow!</p>
-      <Link href="/sign-in" className="mt-8">
-        <Button className="md:py-6 md:px-7 md:text-base">Get Started</Button>
-      </Link>
+    <div>
+      <Navbar />
+      <Hero />
+      <Features />
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useIsMobile } from '@/hooks/use-mobile'
+import Image from 'next/image'
+import React from 'react'
+
+function Hero() {
+  const isMobile = useIsMobile();
+
+  return (
+    <div className='mt-16 flex flex-col items-center justify-center gap-4'>
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-600 to-white text-4xl md:text-7xl font-sans relative z-20 font-bold tracking-tight">
+        Manage Finances <br /> Easily and Smartly.
+      </h2>
+      <p className='text-neutral-300 text-center mx-4'>FinTrack helps you control spending, track income, and manage savings with an intuitive financial dashboard.</p>
+
+      <div className='md:mt-16 mt-8 relative flex justify-center items-center'>
+        <div className='overflow-hidden border-2 border-white rounded-lg'>
+          <Image src={'/hero.png'} height={555} width={!isMobile ? 1080 : 350} alt='Dashboard Image' />
+          <div className="absolute inset-0 bg-gradient-to-b from-10% to-90% from-transparent to-black/80" />
+        </div>
+        <div className='bg-purple-500/50 h-full md:w-[71rem] w-full absolute -z-1 blur-2xl' />
+      </div>
+      <div className='w-full h-20 -mt-16 blur-xl relative -z-10 bg-purple-500/70' />
+    </div>
+  )
+}
+
+export default Hero
