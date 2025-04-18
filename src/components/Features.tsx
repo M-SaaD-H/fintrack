ec5873261afb59d78f65e2df2675ebdb22800c4a
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-04-18 12:34:13 +0530

    minor tweak

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -32,7 +32,7 @@ const items = [
 
 function Features() {
   return (
-    <section id='features' className='bg-black -mt-16 p-16 relative z-50 w-full h-44'>
+    <section id='features' className='bg-black -mt-16 p-16 relative z-50 w-full'>
       <h2 className='md:text-6xl text-2xl font-sans font-semibold tracking-tight text-center text-nowrap my-4'>Powerful Features to Elevate <br /> You Fianncially</h2>
       <p className='text-neutral-300 text-center max-w-md text-wrap mx-auto'>All the tools you need to manage your money - smart, simple and seamless.</p>
       <BentoGrid className="max-w-5xl mx-auto md:auto-rows-[20rem] my-8">
