1a6725243b216b82e6aa2f084925e6547468ec09
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2025-04-18 22:10:15 +0530

    some minor changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,9 +17,9 @@ function Hero() {
       <div className='md:mt-16 mt-8 relative flex justify-center items-center'>
         <div className='overflow-hidden border-2 border-white rounded-lg'>
           <Image src={'/hero.png'} height={555} width={!isMobile ? 1080 : 350} alt='Dashboard Image' className='h-auto w-auto' />
-          <div className="absolute inset-0 bg-gradient-to-b from-10% to-90% from-transparent to-black/80" />
+        <div className="absolute inset-0 bg-gradient-to-b from-10% to-90% from-transparent to-black/80" />
         </div>
-        <div className='bg-purple-500/50 h-[33rem] md:w-[63rem] w-full absolute -z-1 blur-2xl' />
+        <div className='bg-purple-500/50 h-[13rem] md:h-[33rem] md:w-[63rem] w-full absolute -z-1 blur-2xl' />
       </div>
       <div className='w-full h-20 -mt-16 blur-xl relative -z-10 bg-purple-500/70' />
     </div>
